f9552d25d181b483b577193f3a7a4583d5d13ecd
Julien-Allard <julalldev@gmail.com>
2022-02-16 17:37:37 +0100

    Third commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,20 @@
+import "../components/header.css";
+import reactLogo from "../assets/img/react-brands.svg";
+
 const Header = () => {
-  return <div>pouet</div>;
+  return (
+    <div className="header-container">
+      <div className="banner-full">
+        <div className="banner-logo">
+          <h1>Mar-Veact</h1>
+          <div>
+            <img src={reactLogo} alt="" className="react-logo" />
+          </div>
+        </div>
+        <p>Full Marvel / Full React</p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
