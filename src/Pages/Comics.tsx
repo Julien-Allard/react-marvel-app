3c9b0d83baffb74bf813a8b0f682b3f2cde6bb6b
Julien-Allard <julalldev@gmail.com>
2022-07-12 14:43:36 +0200

    Feat: Comics.tsx

--- src/Pages/Comics.tsx
+++ src/Pages/Comics.tsx
@@ -1,19 +1,45 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "../components/comics.css";
-import PaginationTwo from "../components/PaginationTwo";
-import Searchbar from "../components/Searchbar";
+import axios from 'axios';
+import { useState, useEffect, Dispatch, SetStateAction, FC } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHandBackFist } from '@fortawesome/free-solid-svg-icons';
+import '../components/comics.css';
+import PaginationTwo from '../components/PaginationTwo';
+import Searchbar from '../components/Searchbar';
 
-const Comics = ({ search, setSearch }) => {
-  const [comicsData, setComicsData] = useState();
-  const [comicsPage, setComicsPage] = useState(1);
-  const [comicsMaxPage, setComicsMaxPage] = useState();
-  const [comicsLoading, setComicsLoading] = useState(true);
-  const [isFavourite, setIsFavourite] = useState(false);
+interface ComicsProps {
+  search: string;
+  setSearch: Dispatch<SetStateAction<string>>;
+}
 
-  const handleFavourites = (value) => {
-    let favContainer;
+type Thumbnail = {
+  path: string;
+  extension: string;
+};
+
+type ComicsDatasResults = {
+  thumbnail: Thumbnail;
+  _id: string;
+  title: string;
+  description: string;
+};
+
+type ReceivedComicsDatas = {
+  count: number;
+  limit: number;
+  results: ComicsDatasResults[];
+};
+
+const Comics: FC<ComicsProps> = ({ search, setSearch }) => {
+  const [comicsData, setComicsData] = useState<ReceivedComicsDatas | null>(
+    null,
+  );
+  const [comicsPage, setComicsPage] = useState<number>(1);
+  const [comicsMaxPage, setComicsMaxPage] = useState<number | null>(null);
+  const [comicsLoading, setComicsLoading] = useState<boolean>(true);
+  const [isFavourite, setIsFavourite] = useState<boolean>(false);
+
+  const handleFavourites = (value: ComicsDatasResults) => {
+    let favContainer: typeof favObject[];
     const favObject = {
       thumbnail: {
         path: value.thumbnail.path,
@@ -24,30 +50,29 @@ const Comics = ({ search, setSearch }) => {
       description: value.description,
     };
 
-    if (localStorage.getItem("favcomics")) {
-      const importFavs = localStorage.getItem("favcomics");
+    if (localStorage.getItem('favcomics')) {
+      const importFavs = localStorage.getItem('favcomics') || ''; // need to put || "" because localStorage return type can be string or null
       favContainer = JSON.parse(importFavs);
       const filter = favContainer.filter((elem) => elem._id === value._id);
-      // console.log(filter);
 
       if (filter.length > 0) {
         for (let i = 0; i < favContainer.length; i++) {
           if (favContainer[i]._id === value._id) {
             favContainer.splice(favContainer.indexOf(favContainer[i]), 1);
             const exportFavs = JSON.stringify(favContainer);
-            localStorage.setItem("favcomics", exportFavs);
+            localStorage.setItem('favcomics', exportFavs);
           }
         }
       } else {
         favContainer.push(favObject);
         const exportFavs = JSON.stringify(favContainer);
-        localStorage.setItem("favcomics", exportFavs);
+        localStorage.setItem('favcomics', exportFavs);
       }
     } else {
       favContainer = [];
       favContainer.push(favObject);
       const exportFavs = JSON.stringify(favContainer);
-      localStorage.setItem("favcomics", exportFavs);
+      localStorage.setItem('favcomics', exportFavs);
     }
     setIsFavourite(!isFavourite);
   };
@@ -56,12 +81,12 @@ const Comics = ({ search, setSearch }) => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `https://marveact-api.herokuapp.com/comics?page=${comicsPage}&title=${search}`
+          `https://marveact-api.herokuapp.com/comics?page=${comicsPage}&title=${search}`,
         );
         setComicsData(response.data);
         setComicsMaxPage(Math.ceil(response.data.count / response.data.limit));
         setComicsLoading(false);
-      } catch (error) {
+      } catch (error: any) {
         console.log(error.response);
       }
     };
@@ -79,7 +104,7 @@ const Comics = ({ search, setSearch }) => {
       <h1>Comics collection</h1>
       <p className="comics-intro">
         Here's the fun part. You like heroes ? And you like to read ? Just take
-        a look at the {comicsData.count} references and see what's going on !
+        a look at the {comicsData?.count} references and see what's going on !
         Feel free to click on any of them to add it as favourite.
       </p>
       <Searchbar search={search} setSearch={setSearch} />
@@ -89,7 +114,7 @@ const Comics = ({ search, setSearch }) => {
         comicsMaxPage={comicsMaxPage}
       />
       <div className="comics-card-container">
-        {comicsData.results.map((comics) => {
+        {comicsData?.results.map((comics: ComicsDatasResults) => {
           return (
             <div
               className="comics-card"
@@ -110,10 +135,10 @@ const Comics = ({ search, setSearch }) => {
               ) : (
                 <p>Description not available</p>
               )}
-              {localStorage.getItem("favcomics") &&
-                localStorage.getItem("favcomics").includes(comics._id) && (
+              {localStorage.getItem('favcomics') &&
+                localStorage?.getItem('favcomics')?.includes(comics._id) && (
                   <FontAwesomeIcon
-                    icon="fa-solid fa-hand-back-fist"
+                    icon={faHandBackFist}
                     className="comics-bookmark-icon"
                   />
                 )}
